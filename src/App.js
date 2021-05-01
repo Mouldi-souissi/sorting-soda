39e6c8ed8f55827a8725dadf3404085ad1cab7e7
unknown <souissi13mouldi@gmail.com>
2021-05-01 20:12:05 +0100

    win

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useContext } from "react";
 import "./App.css";
 import Tubes from "./components/Tubes";
+import { gameContext } from "./context/gameContext";
 
 function App() {
+  const { start, win } = useContext(gameContext);
   return (
     <div className="App">
       <div className="container">
-        <h1 className="my-5 text-center title">Soda Sorting</h1>
+        <h1 className="mt-5 text-center title">Soda Sorting</h1>
+
         <Tubes />
+        <div className="d-flex">
+          {win && (
+            <button
+              className="btn"
+              style={{ fontSize: "20px" }}
+              onClick={start}
+            >
+              Next level
+            </button>
+          )}
+
+          {win && <h2 className="ml-auto">Bravo !</h2>}
+        </div>
       </div>
     </div>
   );
--- src/context/gameContext.js
+++ src/context/gameContext.js
@@ -6,7 +6,9 @@ export const gameContext = createContext();
 const GameContextProvider = (props) => {
   const [bottles, setBottles] = useState("");
   const [fromTo, setFromTo] = useState([]);
-  // const [win, setWin] = useState(true);
+  const [totalBottles] = useState(4);
+  const [maxLevel] = useState(4);
+  const [win, setWin] = useState(false);
 
   // transfer lequid from to bottle
   const setDestination = (bottle) => {
@@ -18,39 +20,49 @@ const GameContextProvider = (props) => {
     }
   };
 
+  // check for win
+  const checkWin = () => {
+    let check = true;
+    let fullBottles = bottles.filter(
+      (bottle) => bottle.inner.length === maxLevel
+    );
+
+    let res = fullBottles.map((bottle, i) => {
+      if (bottle.inner[i] !== bottle.inner[i + 1]) {
+        return (check = false);
+      } else return check;
+    });
+
+    if (res.filter((el) => el).length === totalBottles - 2) {
+      setWin(true);
+    }
+  };
+
   // transfer
   useEffect(() => {
     if (fromTo.length === 2) {
       soda.transfer(fromTo[0], fromTo[1], bottles, 4);
       setFromTo([]);
+      checkWin();
     }
   }, [fromTo, bottles]);
 
+  // start
+  const start = () => {
+    let bottles = soda.createSoda(totalBottles, maxLevel);
+    setBottles(bottles);
+    setWin(false);
+  };
+
   // load bottles
   useEffect(() => {
-    let bottles = soda.createSoda(6, 4);
-    setBottles(bottles);
+    start();
   }, []);
 
-  // check for win
-  // useEffect(() => {
-
-  //   if (bottles.length) {
-  //     bottles
-  //       .filter((bottle) => bottle.inner.length)
-  //       .map((bottle) => {
-  //         bottle.inner.map((a, b) => {
-  //           if (a !== b) {
-  //             setWin(false);
-  //           }
-  //         });
-  //       });
-  //   }
-
-  // }, [bottles]);
-
   return (
-    <gameContext.Provider value={{ bottles, setDestination, fromTo }}>
+    <gameContext.Provider
+      value={{ bottles, setDestination, fromTo, start, win }}
+    >
       {props.children}
     </gameContext.Provider>
   );
